8f9cda1f3721292b3c723d9a83b295ab6fd10eda
Metaimperiya <ddos.atack.service@gmail.com>
2026-07-25 05:00:36 +0300

    Create tombs.js

---
+++ assets/js/cemetery/tombs.js
@@ -0,0 +1,9 @@
+/**
+ * Памятники кладбища
+ * Все типы: классический, крест, двойной, голографический
+ */
+
+export function createCemetery() {
+    // TODO: Создание всех памятников
+    console.log('✅ Cemetery created');
+}
