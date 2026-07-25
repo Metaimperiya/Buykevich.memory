c513294f1f1a6d3244e56b33099e43dba0b827c9
Metaimperiya <ddos.atack.service@gmail.com>
2026-07-25 05:01:22 +0300

    Create video-player.js

---
+++ assets/js/media/video-player.js
@@ -0,0 +1,9 @@
+/**
+ * Видеоплеер для 3D-экранов
+ * Поддерживает видео в галактиках и на стенах
+ */
+
+export function createVideoPlayer(videoUrl) {
+    // TODO: Создание видеоплеера
+    console.log(`🎬 Video player: ${videoUrl}`);
+}
