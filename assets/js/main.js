cfa7dc0a43f8b04a9e30ab78169c86cdc51a5fd2
Metaimperiya <ddos.atack.service@gmail.com>
2026-07-25 04:59:47 +0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Memorial Park — Главный входной файл
+ * 
+ * Сборка всех модулей в единое приложение
+ */
+
+// TODO: Импортировать модули после разбивки
+// import { initScene } from './core/scene.js';
+// import { createCemetery } from './cemetery/tombs.js';
+// import { createGalaxy } from './galaxy/galaxy-system.js';
+// import { initControls } from './controls/fps-camera.js';
+// import { initAudio } from './media/audio-controller.js';
+
+console.log('🪦 Memorial Park — Initializing...');
+console.log('📁 Project structure ready!');
+console.log('📷 Add photos to assets/photos/');
+console.log('🎬 Add videos to assets/videos/');
+console.log('🎵 Add audio to assets/audio/');
