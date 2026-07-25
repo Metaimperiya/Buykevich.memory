13cd76de8f381c6d57b69ae93971e3841b67c5a8
Metaimperiya <ddos.atack.service@gmail.com>
2026-07-25 05:02:05 +0300

    Create audio-controller.js

---
+++ assets/js/media/audio-controller.js
@@ -0,0 +1,9 @@
+/**
+ * Аудио-контроллер
+ * Фоновая музыка и звуковые эффекты
+ */
+
+export function initAudio() {
+    // TODO: Инициализация аудио
+    console.log('🎵 Audio controller initialized');
+}
